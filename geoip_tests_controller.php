8996e0d1a44f52f359ac58629997142115e9766f
Wayne Khan <kzhiwei@wayland.(none)>
2009-12-07 16:17:45 +0800

    Pretty-printing.

--- geoip_tests_controller.php
+++ geoip_tests_controller.php
@@ -1,17 +1,14 @@
 <?php
 class GeoipTestsController extends AppController {
-
 	var $components = array("Geoip");
 	var $uses = array();
 
-	function countryCode($ip_address = null)
-	{
+	function countryCode($ip_address = null) {
 		pr($this->Geoip->countryCode($ip_address));
 		exit;
 	}
 
-	function countryName($ip_address = null)
-	{
+	function countryName($ip_address = null) {
 		pr($this->Geoip->countryCode($ip_address));
 		exit;
 	}
